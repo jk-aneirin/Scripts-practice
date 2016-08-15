b02c3be0b7dbfe48fc356a84149f831b6c479911
xl <xl@server3.wm.org>
2016-08-15 10:04:57 -0400

    add fuzzyfinder.py&README

---
+++ fuzzyfinder.py
@@ -0,0 +1,11 @@
+import re
+def fuzzyfinder(user_input,collection):
+    suggestions=[]
+    pattern='.*?'.join(user_input)
+    regex=re.compile(pattern)
+    for item in collection:
+        match=regex.search(item)
+        if match:
+            suggestions.append((len(match.group()),match.start(),item))
+    return [x for _,_,x in sorted(suggestions)]
+
