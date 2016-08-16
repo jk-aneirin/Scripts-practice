3390ad3d88ba1712246e00222182ccd464871230
jk-aneirin <bigxuliang@sina.com>
2016-08-16 12:53:13 +0800

    Update fuzzyfinder.py

--- fuzzyfinder.py
+++ fuzzyfinder.py
@@ -1,7 +1,7 @@
 import re
 def fuzzyfinder(user_input,collection):
     suggestions=[]
-    pattern='.*?'.join(user_input)
+    pattern='.*?'.join(user_input) //使用非贪婪匹配
     regex=re.compile(pattern)
     for item in collection:
         match=regex.search(item)
